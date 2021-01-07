c35b9944e4b046807159593f9b24dd69dea1e328
DiangD <1304138666@qq.com>
2021-01-07 23:12:52 +0800

    Bridge:add example

--- src/main/java/com/qzh/design/pattern/structural/bridge/Bank.java
+++ src/main/java/com/qzh/design/pattern/structural/bridge/Bank.java
@@ -5,7 +5,7 @@ package com.qzh.design.pattern.structural.bridge;
  * @Author DiangD
  * @Date 2021/1/6
  * @Version 1.0
- * @Description 抽象
+ * @Description 抽象 桥接模式参考jdk Driver DriverInfo DriverManager
  **/
 public abstract class Bank {
     protected Account account;
